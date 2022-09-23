2d5e93bef57ce09fc7f148012f17ff3ff4d99a1a
Dhruv Pant <dhruv@gopokyo.com>
2022-09-23 18:03:10 +0530

    process commands on messagae

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-import string
-
 import discord
 import os
+from discord.ext import commands
 from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+
+client = commands.Bot(command_prefix='/', intents=intents)
+
 
 @client.event
 async def on_ready():
@@ -27,10 +27,18 @@ async def on_message(message):
             await message.channel.send('Hello! {0}'.format(message.author))
         else:
             await message.channel.send('Hello! {0}'.format(message.author.nick))
-        return
+
+    await client.process_commands(message)
 @client.event
 async def on_member_join(member):
     await member.send('Listen here you little bitch...{0}'.format(member.name))
     return
+
+
+@client.command(help = 'Get yourself sucked off', breif= 'suck')
+async def suck(ctx):
+    await ctx.channel.send('Sucking...')
+    return
+
 
 client.run(TOKEN);
